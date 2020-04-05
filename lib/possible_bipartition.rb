0b5b167f1dff6155210339b36784eed6358a1cd4
Julia Bouvier <juliabouv@gmail.com>
2020-04-05 13:29:25 -0700

    Implemented possible_bipartition

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,42 @@
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  return true if dislikes.empty?
+
+  i = -1
+  unvisited = dislikes.map { i += 1 }
+  queue = [0]
+
+  red = Set[0]
+  green = Set[]
+
+  until queue.empty? && unvisited.empty? do
+    if queue.empty?
+      current = unvisited.pop()
+    else
+      current = queue.shift
+      unvisited.delete(current)
+    end
+    p red
+    p green
+
+    dislikes[current].each do |neighbor|
+      if red.include?(current)
+        return false if red.include?(neighbor)
+
+        unless green.include?(neighbor)
+          green.add(neighbor)
+          queue << neighbor
+        end
+      else
+        return false if green.include?(neighbor)
+
+        unless red.include?(neighbor)
+          red.add(neighbor)
+          queue << neighbor
+        end
+      end
+    end
+  end
+
+  return true
 end
